caf73c29746c0464a5c5026e4fe90df81a64ee42
Adnan RIHAN <adnan@rihan.fr>
2020-05-26 12:08:36 +0200

    Funtionnal commit

---
+++ src/Provider.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Max13\TelegramSocialite;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use InvalidArgumentException;
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Telegram bot username
+     *
+     * @var string
+     */
+    protected $botname;
+
+    /**
+     * Set botname
+     *
+     * @param  string $botname
+     * @return void
+     */
+    public function setBotname($botname)
+    {
+        $this->botname = $botname;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        return null;
+    }
+
+    /**
+     * Return the login button in HTML
+     *
+     * @return string
+     */
+    public function getButton()
+    {
+        $botname = $this->botname;
+        $callbackUrl = $this->redirectUrl;
+
+        return '<script async src="https://telegram.org/js/telegram-widget.js" data-telegram-login="'.$botname.'" data-size="large" data-userpic="false" data-auth-url="'.$callbackUrl.'" data-request-access="write"></script>';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function redirect()
+    {
+        return '<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+
+        <title>Login using Telegram</title>
+    </head>
+    <body>
+        '.$this->getButton().'
+    </body>
+</html>';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'        => $user['id'],
+            'nickname'  => $user['username'],
+            'name'      => trim($user['first_name'].' '.$user['last_name']),
+            'avatar'    => $user['photo_url'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        $validator = Validator::make($this->request->all(), [
+            'id'        => 'required|numeric',
+            'auth_date' => 'required|date_format:U|before:1 day',
+            'hash'      => 'required|size:64',
+        ]);
+
+        throw_if($validator->fails(), InvalidArgumentException::class);
+
+        $dataToHash = collect($this->request->except('hash'))
+                        ->transform(function ($val, $key) {
+                            return "$key=$val";
+                        })
+                        ->sort()
+                        ->join("\n");
+
+        $hash_key = hash('sha256', $this->clientSecret, true);
+        $hash_hmac = hash_hmac('sha256', $dataToHash, $hash_key);
+
+        throw_if(
+            $this->request->hash !== $hash_hmac,
+            InvalidArgumentException::class
+        );
+
+        return $this->mapUserToObject($this->request->except(['auth_date', 'hash']));
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Max13\TelegramSocialite;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Laravel\Socialite\Contracts\Factory as Socialite;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $socialite = $this->app->make(Socialite::class);
+        $socialite->extend(
+            'telegram',
+            function ($app) use ($socialite) {
+                $config = $app['config']['services.telegram'];
+                $provider = $socialite->buildProvider(Provider::class, $config);
+                $provider->setBotname($config['botname']);
+                return $provider;
+            }
+        );
+    }
+}
